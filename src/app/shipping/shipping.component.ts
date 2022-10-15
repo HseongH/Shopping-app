541521b921dc83efc91d5d9cf40254dd0781a4a4
HseongH <hh4518@gmail.com>
2022-10-15 17:16:34 +0900

    배송 정보 확인 컴포넌트 구현 완료

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.css'],
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts = this.cartService.getShoppingPrices();
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
